600ea0058e249b38acffd4891d9828e118977c9e
gotouchgra55 <shauryatamang2009@gmail.com>
2025-08-17 04:05:27 +0000

    Migrate the code to express

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 import path from 'path';
-import http from 'http';
 import url from 'url';
-import fs from 'fs/promises';
+import express from 'express';
 
 const PORT = process.env.PORT;
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const server = http.createServer(async (req, res) => {
-  try {
-    if (req.method === 'GET') {
-      let filepath;
-      if (req.url === '/') {
-        filepath = path.join(__dirname, 'public', 'index.html');
-      } else if (req.url === '/contact') {
-        filepath = path.join(__dirname, 'public', 'contact-me.html');
-      } else if (req.url === '/about') {
-        filepath = path.join(__dirname, 'public', 'about.html');
-      } else {
-        filepath = path.join(__dirname, 'public', '404.html');
-      }
-      const data = await fs.readFile(filepath);
-      res.setHeader('Content-Type', 'text/html');
-      res.end(data);
-    } else {
-      throw new Error('Unauthorized method!');
-    }
-  } catch (err) {
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
-    res.end('Server error');
-  }
-});
+const app = express();
 
-server.listen(PORT, () => {
-  console.log(`Started server on port: ${PORT}`);
-});
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'contact-me.html'));
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+})
+
+app.use((req, res) => {
+  res.status(404);
+  res.sendFile(path.join(__dirname, 'public', '404.html'));
+})
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw new Error(error);
+  }
+  console.log(`Server started on port: ${PORT}`);
+})
